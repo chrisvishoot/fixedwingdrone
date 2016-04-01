4501e286546a42b4c821db470b69a2502b88deaa
Rahul <deshpr@uw.edu>
2016-04-01 10:21:22 -0700

    Removed directory

--- FlightControl/RPI_Handshake.v
+++
@@ -1,6 +0,0 @@
-module RPI_Handshake(RPI_Ack, Mode);
-	input [0:0] RPI_Ack;
-	output [0:0] Mode;
-	wire [0:0] Mode;
-	assign Mode= RPI_Ack;
-endmodule
--- FlightControl/UART_Handshake.v
+++
@@ -1,2 +0,0 @@
-module RPI_Handshake();
-endmodule
